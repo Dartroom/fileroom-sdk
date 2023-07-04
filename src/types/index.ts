e33907eaf261ed01a666f255215f47f923c6a0e2
Spencerjibz <=spencernajib2@gmail.com>
2023-07-04 14:15:09 +0100

    add Fileroom ResponseType

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,19 @@
 export type RequestData = Record<string, any>;
-export type RequestHeaders = Record<string, string | number | string[]>| typeof Request;
+export type RequestHeaders =
+  | Record<string, string | number | string[]>
+  | typeof Request;
 export type ResponseHeaderValue = string | string[];
-export type ResponseHeaders = Record<string, ResponseHeaderValue>| typeof Headers;
+export type ResponseHeaders =
+  | Record<string, ResponseHeaderValue>
+  | typeof Headers;
 export type TimeoutError = TypeError & { code?: string };
+
+export type FileroomError = Array<{
+  data?: {
+    message: string;
+  };
+  message: string;
+  source?: string;
+  status?: number;
+}>;
+
