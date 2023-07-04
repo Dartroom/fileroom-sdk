2731a6b983a8eafd6ea2dcecd4720f01ad17c8cb
Spencerjibz <=spencernajib2@gmail.com>
2023-07-04 14:10:03 +0100

    add ipfs/cid to fetch files

--- src/api/ipfs/index.ts
+++ src/api/ipfs/index.ts
@@ -1,6 +1,10 @@
 import { BaseApi } from '../baseApi';
-import { statusResponse } from '../../interfaces';
-
+import {
+  statusResponse,
+  getResponse,
+  browserRawResponse,
+} from '../../interfaces';
+import { isBrowser } from 'browser-or-node';
 /**
  *  IPFS  endpoint for the Fileroom API
  */
@@ -18,5 +22,47 @@ export class IpfsApi extends BaseApi {
     let json: any = await response.toJSON();
 
     return json as statusResponse;
+  }
+
+  /** get a file from the gateway
+   * @param cid
+   * */
+  async get(cid: string): Promise<getResponse> {
+    const response = await this.createHttpRequest.makeRequestwithDefault(
+      '/ipfs/' + cid,
+      'GET',
+    );
+    let headers: any = response.getHeaders();
+    let contentType = headers['content-type'];
+    let fileLength = headers['content-length'];
+    let metatdata = {
+      contentType: contentType,
+      contentLength: fileLength,
+    };
+    let result = {} as getResponse;
+
+    let stream = null;
+    if (contentType.includes('application/json')) {
+      let json = await response.toJSON();
+      result.errors = json.errors.map((v: any) => {
+        v.status = 404;
+        v.message = 'File not found';
+        return v;
+      });
+      return result;
+    }
+    result.metatdata = metatdata;
+    if (isBrowser) {
+      let raw = (await response.getRawResponse()) as browserRawResponse;
+      console.log(raw);
+
+      stream = raw._bodyInit.stream();
+      result.stream = stream;
+      return result;
+    } else {
+      stream = response.toStream(() => {}) as ReadableStream<Uint8Array>;
+      result.stream = stream;
+      return result;
+    }
   }
 }
--- src/interfaces/ipfs.ts
+++ src/interfaces/ipfs.ts
@@ -1,3 +1,4 @@
+import { FileroomError } from '../types';
 export interface statusResponse {
   peername: string;
   status: string;
@@ -9,4 +10,25 @@ export interface statusResponse {
   priority_pin: boolean;
   metadata: any | null;
   created: string;
+}
+
+export interface getResponse {
+  errors?: FileroomError;
+  stream?: ReadableStream<Uint8Array> | null;
+  metatdata?: meta;
+}
+
+interface meta {
+  contentType: string;
+  contentLength: number;
+}
+
+export interface browserRawResponse {
+  _bodyBlob: Blob;
+  _bodyInit: Blob;
+  type: string;
+  url: string;
+  statusText: string;
+  ok: boolean;
+  headers: Headers;
 }
