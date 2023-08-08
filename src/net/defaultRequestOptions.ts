9feef5fcebfcff05092168d0d438a5f875641b84
Spencerjibz <=spencernajib2@gmail.com>
2023-08-08 19:24:54 +0100

    Increase timeout for default request options in prod and beta environments

--- src/net/defaultRequestOptions.ts
+++ src/net/defaultRequestOptions.ts
@@ -12,12 +12,12 @@ export const ProdOpts: RequestOptions = {
   host: 'beta-ams-1.fileroom.app',
   path: '/',
   protocol: 'https',
-  timeout: 60000,
+  timeout: 120000,
 };
 
 export const BetaOpts: RequestOptions = {
   host: 'beta.fileroom.app',  
   path: '/',
   protocol: 'https',
-  timeout: 60000,
+  timeout: 120000,
 };
