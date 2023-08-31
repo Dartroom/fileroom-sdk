007b63a840df48f19a79308a7cfa642f48942e2a
Spencerjibz <=spencernajib2@gmail.com>
2023-08-31 20:49:42 +0100

    \ No newline at end of fileAdd AccountType enum to index.ts

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -4,11 +4,15 @@ export const enum PinStatus {
   UNPINNED = 'unpinned',
 }
 
-
 export const enum FileType {
   IMAGE = 'image',
   ANIMATION = 'animation',
   VIDEO = 'video',
   ANY = 'any',
   UNSUPPORTED = 'unsupported',
+}
+
+export const enum AccountType {
+  DARTROOM = 'DARTROOM',
+  FILEROOM = 'EXTERNAL',
 }
